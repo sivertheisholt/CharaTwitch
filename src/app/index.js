76c2ea82e49d0d8753e06fedfaa0efa3fc08b58f
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-11-09 19:34:04 +0100

    Fixed chromium for windows

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,35 @@
 const CaiController = require("../app/controllers/caiController");
 const TwitchController = require("../app/controllers/twitchController");
+const puppeteer = require("@puppeteer/browsers");
+const decompress = require("decompress");
+const fs = require("fs");
 
-// eslint-disable-next-line no-undef
-new CaiController(caiView);
-// eslint-disable-next-line no-undef
-new TwitchController(twitchView, caiView);
+async function startApp() {
+  const buildId = "114.0.5735.133"; //which version you want to download
+
+  await puppeteer.install({
+    cacheDir: "chrome-cache/",
+    unpack: false,
+    browser: "chrome",
+    platform: "win32", //or linux, mac, mac_arm, win64
+    buildId,
+  });
+
+  decompress("chrome-cache/chrome/114.0.5735.133-chrome-win32.zip", ".")
+    .then(() => {
+      console.log("Unzipped");
+      fs.rmSync("chrome-cache", { recursive: true, force: true });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  // eslint-disable-next-line no-undef
+  new CaiController(caiView);
+  // eslint-disable-next-line no-undef
+  new TwitchController(twitchView, caiView);
+
+  console.log("done");
+}
+
+startApp();
--- src/app/services/cai/caiService.js
+++ src/app/services/cai/caiService.js
@@ -11,7 +11,7 @@ const authService = require("../authService");
 class CaiService {
   constructor() {
     this.characterAi = new CharacterAI();
-    this.characterAi.requester.puppeteerPath = "./chrome-win/chrome";
+    this.characterAi.requester.puppeteerPath = "./chrome-win32/chrome";
     this.caiApiService = caiApiService(this.characterAi);
     this.authService = authService();
   }
